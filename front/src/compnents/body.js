05d3620533f520bc5f8086de75a47e192687bd6b
Jacob Hein <jacobhein1892@gmail.com>
2023-02-12 19:07:49 -0800

    going to start working on layout for front page

--- front/src/compnents/body.js
+++ front/src/compnents/body.js
@@ -5,21 +5,23 @@ export default function Body({userInfo})
 {
     return (
         <div className='wrapper'>
-            <h1>Main Section of the App</h1>
-            <p>I'm going to have various functions here in order to set up the functionality to pass to the backend.</p>
-            <hr/>
-            <p>League_ID: {userInfo.league_id ? userInfo.league_id : "" }</p>
-            <p>Team Name: {userInfo.team_name ? userInfo.team_name : "" }</p>
-            <hr/>
-            <p>League_id (This will need to be an input)</p>
-            <p>Team name</p>
-            <p>Create a section to see which players are on waivers. Maybe see which ones would be value adds for your team</p>
-            <p>Create section to see Free Agents</p>
-            <p>Create section to show matchup for current week</p>
-            <p>See section that has proposed trades</p>
-            <p>Section for the teams roster</p>
-            <p>Section for standings</p>
-
+            <div>
+                <p>My Team Roster</p>
+                <p>League_ID: {userInfo.league_id ? userInfo.league_id : "" }</p>
+                <p>Team Name: {userInfo.team_name ? userInfo.team_name : "" }</p>
+            </div>
+            <div>
+                <p>League Standing</p>
+            </div>
+            <div>
+                <p>Free Agents/Waivers</p>
+            </div>
+            <div>
+                <p>This Weeks matchups</p>
+            </div>
+            <div>
+                <p>Proposed Trades</p>
+            </div>
         </div>
     )
 }
