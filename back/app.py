c2c12dddb868d109f893c6a782f0bb85f01ea1e1
Jacob Hein <jacobhein1892@gmail.com>
2023-02-14 20:28:02 -0800

    set up dummy team for building front end

--- back/app.py
+++ back/app.py
@@ -8,14 +8,69 @@ from flask_cors import CORS
 # selected_position: the selected position for the player
 # status: injury status
 
-myPlayer = [{'player_id': 8578, 'name': 'John Doe', 'position_type': 'B',
- 'eligible_positions': ['C','1B'], 'selected_position': 'C',
+myPlayer = [
+    #Position Players
+    {'player_id': 8578, 'name': 'Ronald Acuna Jr.', 'position_type': 'B',
+ 'eligible_positions': ['OF'], 'selected_position': 'OF',
  'status': ''},
- {'player_id': 8967, 'name': 'Joe Baseball', 'position_type': 'B',
+    {'player_id': 8578, 'name': 'Julio Rodriguez', 'position_type': 'B',
+ 'eligible_positions': ['OF'], 'selected_position': 'OF',
+ 'status': ''},
+    {'player_id': 8967, 'name': 'Aaron Judge', 'position_type': 'B',
+ 'eligible_positions': ['OF'], 'selected_position': 'OF',
+ 'status': ''},
+    {'player_id': 8967, 'name': 'Trea Turner', 'position_type': 'B',
  'eligible_positions': ['SS'], 'selected_position': 'SS',
- 'status': 'DTD'},
- {'player_id': 9961, 'name': 'Ed Reliever', 'position_type': 'P',
- 'eligible_positions': ['RP'], 'status': ''}]
+ 'status': ''},
+    {'player_id': 9961, 'name': 'Jose Ramirez', 'position_type': 'B',
+ 'eligible_positions': ['3B'], 'selected_position':'3B', 'status': ''},
+    {'player_id': 9961, 'name': 'Mookie Betts', 'position_type': 'B',
+ 'eligible_positions': ['OF, 2B'], 'selected_position':'2B', 'status': ''},
+    {'player_id': 8578, 'name': 'Vladamir Guerrero Jr.', 'position_type': 'B',
+ 'eligible_positions': ['1B'], 'selected_position': '1B',
+ 'status': ''},
+    {'player_id': 8967, 'name': 'JT Realmuto', 'position_type': 'B',
+ 'eligible_positions': ['C'], 'selected_position': 'C',
+ 'status': ''},
+    {'player_id': 9961, 'name': 'Bobby Witt Jr', 'position_type': 'B',
+ 'eligible_positions': ['3B', 'SS'], 'selected_position':'util', 'status': ''},
+
+    # Pitchers
+
+    {'player_id': 9961, 'name': 'Corbin Burnes', 'position_type': 'P',
+ 'eligible_positions': ['SP'], 'selected_position':'SP', 'status': ''},
+    {'player_id': 9961, 'name': 'Gerrit Cole', 'position_type': 'P',
+ 'eligible_positions': ['SP'], 'selected_position':'SP', 'status': ''},
+    {'player_id': 9961, 'name': 'Jacob deGrom', 'position_type': 'P',
+ 'eligible_positions': ['SP'], 'selected_position':'SP', 'status': ''},
+    {'player_id': 9961, 'name': 'Spencer Strider', 'position_type': 'P',
+ 'eligible_positions': ['SP', 'RP'], 'selected_position':'RP', 'status': ''},
+    {'player_id': 9961, 'name': 'Edwin Diaz', 'position_type': 'P',
+ 'eligible_positions': ['RP'], 'selected_position':'RP', 'status': ''},
+    {'player_id': 9961, 'name': 'Josh Hader', 'position_type': 'P',
+ 'eligible_positions': ['RP'], 'selected_position':'P', 'status': ''},
+    {'player_id': 9961, 'name': 'Aaron Nola', 'position_type': 'P',
+ 'eligible_positions': ['SP'], 'selected_position':'P', 'status': ''},
+    {'player_id': 9961, 'name': 'Shane McClanahan', 'position_type': 'P',
+ 'eligible_positions': ['SP'], 'selected_position':'P', 'status': ''},
+
+    # Bench
+    {'player_id': 9961, 'name': 'Mike Trout', 'position_type': 'B',
+ 'eligible_positions': ['OF'], 'selected_position':'BN', 'status': ''},
+    {'player_id': 9961, 'name': 'Matt Olson', 'position_type': 'B',
+ 'eligible_positions': ['1B'], 'selected_position':'BN', 'status': ''},
+    {'player_id': 9961, 'name': 'Jose Altuve', 'position_type': 'B',
+ 'eligible_positions': ['2B'], 'selected_position':'BN', 'status': ''},
+
+    # Injured List
+    {'player_id': 9961, 'name': 'Pete Alonso', 'position_type': 'B',
+ 'eligible_positions': ['1B'], 'selected_position':'IL', 'status': 'DTD'},
+    {'player_id': 9961, 'name': 'Michael Harris II', 'position_type': 'B',
+ 'eligible_positions': ['OF'], 'selected_position':'IL:', 'status': 'DTD'},
+    {'player_id': 9961, 'name': 'Dylan Cease', 'position_type': 'B',
+ 'eligible_positions': ['SP'], 'selected_position':'IL', 'status': 'DTD'},
+
+ ]
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
@@ -30,14 +85,11 @@ def home():
 # Used to get league_id and team_name
 @app.route("/", methods=["PUT"])
 def signIn():
-    print("Hello")
     data = request.get_json()
     print(data)
 
-
-
     # Call yahoo_fantasy_api with league_id and team_id to get roster data
-
+    # For now, calling local object of fake team for reference on front
     response = make_response(myPlayer)
     return response
 
