d2dc372c29439a30d25738329fe7a6c8d19450e6
Jacob Hein <jacobhein1892@gmail.com>
2023-02-15 06:46:50 -0800

    commit

--- front/src/compnents/teamRoster.js
+++ front/src/compnents/teamRoster.js
@@ -4,9 +4,9 @@ import rosterStyles from '../styles/teamRoster.modules.css'
 export default function TeamRoster({data}){
     const positionsToBeFilled = ['C', '1B', '2B', '3B', 'SS','OF','OF','OF','Util','SP','SP','SP','RP','RP','P','P','P','BN','BN','BN','IL','IL','IL','IL','NA']
     function generateRosterPositions(){
-            return positionsToBeFilled.map((position) => {
+            return positionsToBeFilled.map((position, index) => {
                 let elegiblePlayer = data.filter((x) => x.selected_position === position);
-                console.log(position, ":", elegiblePlayer);
+                console.log(position, ":", elegiblePlayer, index);
                 if(elegiblePlayer.length === 1){
                     elegiblePlayer = elegiblePlayer[0];
                 } else {
@@ -14,7 +14,7 @@ export default function TeamRoster({data}){
                     elegiblePlayer = elegiblePlayer[0];
                 }
                 return (
-                    <div className='positionSlot'>
+                    <div className='positionSlot' >
                         <p>{position}</p>
                         <p>{elegiblePlayer ? elegiblePlayer.name : '' }</p>
                     </div>
