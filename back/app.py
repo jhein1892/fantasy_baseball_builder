914137dd9d7d5a8a0b6f1ec696482098c060e262
Jacob Hein <jacobhein1892@gmail.com>
2023-02-14 19:54:28 -0800

    working on representing the team received from API on the frontend

--- back/app.py
+++ back/app.py
@@ -1,6 +1,22 @@
 from flask import Flask, make_response, request, jsonify
 from flask_cors import CORS
 
+# player_id: the is for the specific player
+# name: name of player
+# position_type: B for batter, P for pitcher
+# eligible_positions: eligable positions for the player
+# selected_position: the selected position for the player
+# status: injury status
+
+myPlayer = [{'player_id': 8578, 'name': 'John Doe', 'position_type': 'B',
+ 'eligible_positions': ['C','1B'], 'selected_position': 'C',
+ 'status': ''},
+ {'player_id': 8967, 'name': 'Joe Baseball', 'position_type': 'B',
+ 'eligible_positions': ['SS'], 'selected_position': 'SS',
+ 'status': 'DTD'},
+ {'player_id': 9961, 'name': 'Ed Reliever', 'position_type': 'P',
+ 'eligible_positions': ['RP'], 'status': ''}]
+
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
@@ -18,7 +34,11 @@ def signIn():
     data = request.get_json()
     print(data)
 
-    response = make_response({"message": "Successfully updated", "status" : 200})
+
+
+    # Call yahoo_fantasy_api with league_id and team_id to get roster data
+
+    response = make_response(myPlayer)
     return response
 
 if __name__ == "__main__":
