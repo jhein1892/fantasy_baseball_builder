a240fb143af25784727abc00dcc768825eff9516
Jacob Hein <jacobhein1892@gmail.com>
2023-03-02 17:26:49 -0800

    working on a display for weekly matchups

--- front/src/components/matchups.js
+++ front/src/components/matchups.js
@@ -13,7 +13,30 @@ export default function Matchups({data}){
         }
     },[data])
 
-    function generateMatchups(){
+    // function generateMatchups(){
+    //     let availableData = matchupData ? matchupData : {}
+    //     let dataKeys = Object.keys(availableData)
+    //     return dataKeys.map((key) => {
+    //         if(key !== 'count'){
+    //             let singleMatchupData = availableData[key].matchup
+    //             let teamsData = singleMatchupData[0].teams
+    //             let team1 = teamsData[0]['team']
+    //             let team2 = teamsData[1]['team']
+    //             let statData = singleMatchupData['stat_winners']
+
+    //             console.log(team1)
+    //             return (
+    //                 <div className={matchupStyles.matchupContainer}>
+    //                     <p>{team1[0][2]['name']}</p>
+    //                     <p>vs</p>
+    //                     <p>{team2[0][2]['name']}</p>
+    //                 </div>
+    //             )
+    //         }
+    //     })
+    // }
+
+    function generateList(){
         let availableData = matchupData ? matchupData : {}
         let dataKeys = Object.keys(availableData)
         return dataKeys.map((key) => {
@@ -24,9 +47,8 @@ export default function Matchups({data}){
                 let team2 = teamsData[1]['team']
                 let statData = singleMatchupData['stat_winners']
 
-                console.log(team1)
                 return (
-                    <div className={matchupStyles.matchupContainer}>
+                    <div className={matchupStyles.listRow}>
                         <p>{team1[0][2]['name']}</p>
                         <p>vs</p>
                         <p>{team2[0][2]['name']}</p>
@@ -34,11 +56,18 @@ export default function Matchups({data}){
                 )
             }
         })
+
     }
 
     return(
         <div className={matchupStyles.matchupWrapper}>
-                {generateMatchups()}
+            <div className={matchupStyles.listWrapper}>
+                <p>Matchup list</p>
+                {matchupData && generateList()}
+            </div>
+            <div className={matchupStyles.individualWrapper}>
+                <p>Individual Matchups</p>
+            </div>
         </div>
     )
 }
