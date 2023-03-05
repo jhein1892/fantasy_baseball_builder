3a7375ed93830b4e5f9e2d2a2ff995cff9c2f876
Jacob Hein <jacobhein1892@gmail.com>
2023-03-05 08:41:19 -0800

    set up basic form submission for freeagent position

--- front/src/components/freeAgents.js
+++ front/src/components/freeAgents.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function FreeAgents(){
     const positions = ['All Batters','All Pitchers', 'C','1B','2B','3B','SS','LF','CF','RF','SP','RP']
+
+    const [searchValue, setSearchValue] = useState('B')
+
+    function handleChange(e){
+        e.preventDefault()
+        let value = e.target.value;
+        setSearchValue(value)
+    }
+
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(searchValue)
+    }
+
+
     return (
         <div>
             <h1>Free Agents</h1>
-            <select>
-                {positions.map((position) => {
-                    return (
-                        <option>{position}</option>
-                    )
-                })}
-            </select>
-
-            <button>Check Market</button>
+            <form onChange={handleChange} onSubmit={handleSubmit}>                
+                <select>
+                    {positions.map((position, index) => {
+                        let positionValue = position;
+                        if(index < 2){
+                            positionValue = index === 0 ? 'B' : 'P';
+                        }
+                        return (
+                            <option value={positionValue}>{position}</option>
+                        )
+                    })}
+                </select>
+                <button type='submit'>Check Market</button>
+            </form>
         </div>
     )
 }
