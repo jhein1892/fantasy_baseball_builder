cfb5981424cc93b2e23ed9dac2590f18b6578fe5
Jacob Hein <jacobhein1892@gmail.com>
2023-03-06 06:13:28 -0800

    working on pages

--- front/src/components/freeAgents.js
+++ front/src/components/freeAgents.js
@@ -5,6 +5,8 @@ export default function FreeAgents(){
     const positions = ['All Batters','All Pitchers', 'C','1B','2B','3B','SS','LF','CF','RF','SP','RP']
 
     const [searchValue, setSearchValue] = useState('B')
+    const [freeAgentData, setFreeAgentsData] = useState([])
+    const [page, setPage] = useState(1)
 
     function handleChange(e){
         e.preventDefault()
@@ -18,13 +20,36 @@ export default function FreeAgents(){
         axios.put('https://127.0.0.1:5000/freeAgents', {data:searchValue})
         .then((response) => {
           let data = response.data
-          console.log(data)
+          console.log(data.availablePlayers)
+
+          setFreeAgentsData(data.availablePlayers)
         }) 
         .catch((error) => {
           console.log(error)
         })
     }
 
+    function generatePages(){
+        let numValues = freeAgentData ? freeAgentData.length : 0
+        let numPages = Math.floor(numValues / 25)
+        numPages = numValues%25 > 0 ? numPages+1: numPages
+        console.log(numPages)
+        for(let i = 0; i < numPages; i++){
+            return(
+                <p>{i}</p>
+            )
+        }
+    }
+
+    function handlePageChange(e){
+        e.preventDefault()
+        let direction = e.target.name
+        if (direction === 'next'){
+            setPage(page+1)
+        } else {
+            setPage(page-1)
+        }
+    }
 
     return (
         <div>
@@ -43,6 +68,16 @@ export default function FreeAgents(){
                 </select>
                 <button type='submit'>Check Market</button>
             </form>
+            <div>
+                <h3>Player response</h3>
+                <div>
+                    <button name='back' onClick={handlePageChange}>prev</button>
+                    {generatePages()}
+                    <button name='next' onClick={handlePageChange}>next</button>
+                </div>
+            </div>
         </div>
     )
 }
+
+// Sort function, Search function
