6c14119936853ea060528e4b66b258542ecff6d5
Jacob Hein <jacobhein1892@gmail.com>
2023-03-19 09:40:02 -0700

    manipulated the data from the trade proposal in the backend to send to the front

--- back/app.py
+++ back/app.py
@@ -1053,9 +1053,28 @@ def getFreeAgents():
 
 @app.route("/availableTrades")
 def getTrades():
-   pendingTrades = tm.proposed_trades()
+  pendingTrades = tm.proposed_trades()
+  teams = lg.teams()
+  for trade in pendingTrades:
+    trade['tradee_team_key'] = teams[trade['tradee_team_key']]
+    trade['trader_team_key'] = teams[trade['trader_team_key']]
+    tradee_players = trade['tradee_players']
+    tradee_ids = []
+    for player in tradee_players:
+       tradee_ids.append(int(player['player_id']))
+    
+    trader_players = trade['trader_players']
+    trader_ids = []
+    for player in trader_players:
+       trader_ids.append(int(player['player_id']))
+    
+    trade['tradee_players'] = lg.player_details(tradee_ids)
+    trade['trader_players'] = lg.player_details(trader_ids)
 
-   return make_response({"pending_trades": pendingTrades})
+
+  # print(teams)
+
+  return make_response({"pending_trades": pendingTrades})
 
 
 
