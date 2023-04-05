03ac64bfc66015da57226f68a9cc705d7966f7bf
Jacob Hein <jacobhein1892@gmail.com>
2023-04-05 06:13:41 -0700

    working through setting up the header styles

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -4,24 +4,37 @@ import classNames from 'classnames';
 
 function Header({leagueNews}) {
     const [newsType, setNewsType] = useState([])
+    const [displayIndex, setDisplayIndex] = useState(0)
 
     function generateNews(){
-        return newsType.map((type) => {
+        return newsType.map((type, index) => {
+            let newsStyles = classNames(headerStyles.typeWrapper,{
+                [headerStyles.visible]: index === displayIndex
+            })
+
             return (
-                <div className={headerStyles.typeWrapper}>
-                    <h3>{type}</h3>
-                    {
-                    leagueNews[type].map((news, index) => {
-                        return (
-                            <p>{news.transaction_id}</p>
-                        )
-                    })
-                    }
+                <div className={newsStyles}>
+                    <p>{type}</p>
+                    <div className={headerStyles.typeContent}>
+
+                    </div>
                 </div>
-            )
+            )   
+            // return (
+            //     <div className={headerStyles.typeWrapper}>
+            //         <h3>{type}</h3>
+            //         {
+            //         leagueNews[type].map((news, index) => {
+            //             return (
+            //                 <p>{news.transaction_id}</p>
+            //             )
+            //         })
+            //         }
+            //     </div>
+            // )
         })
     }
-
+ 
     useEffect(() => {
         if(leagueNews){
             setNewsType(Object.keys(leagueNews))
@@ -32,7 +45,12 @@ function Header({leagueNews}) {
 
     return (
         <div className={headerStyles.headerWrapper} id='header'>
+            <div className={headerStyles.leftSide}>
+
+            </div>
+            <div className={headerStyles.rightSide}>
                 {generateNews()}
+            </div>
         </div>
     )
 }
